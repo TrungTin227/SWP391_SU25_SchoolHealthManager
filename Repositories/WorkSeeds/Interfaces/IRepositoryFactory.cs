ab50277a4443df2131912a7c8af489b95c868823
TrungTin <tinvtse161572@fpt.edu.vn>
2025-06-07 00:01:22 +0700

    refactor workseeds

---
+++ Repositories/WorkSeeds/Interfaces/IRepositoryFactory.cs
@@ -0,0 +1,11 @@
+﻿namespace Repositories.WorkSeeds.Interfaces
+{
+    public interface IRepositoryFactory // Phải là interface, không phải class
+    {
+        IGenericRepository<TEntity, TKey> GetRepository<TEntity, TKey>()
+            where TEntity : class;
+
+        TRepository GetCustomRepository<TRepository>()
+            where TRepository : class;
+    }
+}
--- Repositories/WorkSeeds/Interfaces/IUnitOfWork.cs
+++ Repositories/WorkSeeds/Interfaces/IUnitOfWork.cs
@@ -1,27 +1,10 @@
-﻿using System.Data;
-using Microsoft.EntityFrameworkCore.Storage;
-
-namespace Repositories.WorkSeeds.Interfaces
+﻿namespace Repositories.WorkSeeds.Interfaces
 {
-    
-    public interface IUnitOfWork : IAsyncDisposable, IDisposable
+
+    public interface IUnitOfWork : IGenericUnitOfWork
     {
-        // Repository properties
         IUserRepository UserRepository { get; }
         IMedicationRepository MedicationRepository { get; }
         IMedicationLotRepository MedicationLotRepository { get; }
-
-        // Transaction management
-        bool HasActiveTransaction { get; }
-
-        Task<IDbContextTransaction> BeginTransactionAsync(
-            IsolationLevel isolationLevel = IsolationLevel.ReadCommitted,
-            CancellationToken cancellationToken = default);
-
-        Task CommitTransactionAsync(CancellationToken cancellationToken = default);
-        Task RollbackTransactionAsync(CancellationToken cancellationToken = default);
-
-        // Save changes
-        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
     }
 }
