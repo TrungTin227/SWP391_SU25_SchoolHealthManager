59ed8da5ec857de78c9e62f3d797a57435f257bb
TaiNguyen2108 <dinhtai2108@gmail.com>
2025-06-18 19:20:38 +0700

    Nurse repo

---
+++ DTOs/NurseDTOs/Request/UpdateNurseRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOs.NurseDTOs.Request
+{
+    public class UpdateNurseRequest
+    {
+        [Required]
+        public Guid UserID { get; set; }  
+
+        [MaxLength(100)]
+        public String? Position { get; set; }     // chức vụ (y tá, y tá trưởng) 
+        
+        [MaxLength(100)]
+        public string? Department { get; set; }         //phòng ban (tiêm chủng, ...) 
+        
+                 
+    }
+}
