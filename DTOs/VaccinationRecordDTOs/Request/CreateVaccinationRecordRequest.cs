bf994de3e67ba5f2c1f59885bd630f8edbef803a
TaiNguyen2108 <dinhtai2108@gmail.com>
2025-06-21 15:17:57 +0700

    Vaccination Record

---
+++ DTOs/VaccinationRecordDTOs/Request/CreateVaccinationRecordRequest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOs.VaccinationRecordDTOs.Request
+{
+    public class CreateVaccinationRecordRequest
+    {
+        [Required]
+        public Guid CampaignId { get; set; }
+
+        [Required(ErrorMessage = "ID học sinh là bắt buộc!")]
+        public Guid StudentId { get; set; }
+
+        [Required(ErrorMessage = "ID của vaccine là bắt buộc!")]
+        public Guid VaccineTypeId { get; set; }
+
+        [Required]
+        public Guid VaccineLotId { get; set; }
+
+        [Required]
+        public DateTime AdministeredDate { get; set; }
+
+        public bool ConsentSigned { get; set; } = false;
+
+        [Required(ErrorMessage = "ID của người tiêm là bắt buộc!")]
+        public Guid VaccinatedBy { get; set; }
+
+        [Required]
+        public DateTime VaccinatedAt { get; set; }
+
+        public bool ReactionFollowup24h { get; set; } = false;
+
+        public bool ReactionFollowup72h { get; set; } = false;
+
+    }
+}
---
+++ DTOs/VaccinationRecordDTOs/Request/UpdateVaccinationRecordRequest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOs.VaccinationRecordDTOs.Request
+{
+    public class UpdateVaccinationRecordRequest
+    {
+        [Required]
+        public Guid Id { get; set; }
+
+        [Required]
+        public Guid CampaignId { get; set; }
+
+        [Required]
+        public Guid StudentId { get; set; }
+
+        [Required]
+        public Guid VaccineTypeId { get; set; }
+
+        [Required]
+        public Guid VaccineLotId { get; set; }
+
+        [Required]
+        public DateTime AdministeredDate { get; set; }
+
+        public bool ConsentSigned { get; set; } = false;
+
+        [Required]
+        public Guid VaccinatedBy { get; set; }
+
+        [Required]
+        public DateTime VaccinatedAt { get; set; }
+
+        public bool ReactionFollowup24h { get; set; } = false;
+
+        public bool ReactionFollowup72h { get; set; } = false;
+
+    }
+}
