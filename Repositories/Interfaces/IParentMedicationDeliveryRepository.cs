77744872a5b6165278e56d3f85546a8e307b1313
hctrung2k4 <hctrung2k4@gmail.com>
2025-06-24 15:49:03 +0700

    adjust api by Phat request, add acpt delivery by nurse id

--- Repositories/Interfaces/IParentMedicationDeliveryRepository.cs
+++ Repositories/Interfaces/IParentMedicationDeliveryRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DTOs.ParentMedicationDeliveryDTOs.Request;
 using DTOs.ParentMedicationDeliveryDTOs.Respond;
 
 namespace Repositories.Interfaces
@@ -12,6 +13,7 @@ namespace Repositories.Interfaces
         Task<List<GetParentMedicationDeliveryRespondDTO>> GetAllParentMedicationDeliveryDTO();
         Task<List<GetParentMedicationDeliveryRespondDTO>> GetAllParentMedicationDeliveryByParentIdDTO(Guid id);
 
+        Task<CreateParentMedicationDeliveryRequestDTO> CreateParentMedicationDeliveryRequestDTO(CreateParentMedicationDeliveryRequestDTO request);
         Task<GetParentMedicationDeliveryRespondDTO?> GetParentMedicationDeliveryByIdDTO(Guid id);
 
 
