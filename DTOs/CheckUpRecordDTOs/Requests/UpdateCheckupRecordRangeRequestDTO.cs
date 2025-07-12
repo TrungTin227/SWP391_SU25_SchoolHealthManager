bd617a67f6df862bdd72b6533ed5082fec9bd35c
hctrung2k4 <hctrung2k4@gmail.com>
2025-07-12 21:49:12 +0700

    Enhance checkup record management with update, restore, and bulk operation support

---
+++ DTOs/CheckUpRecordDTOs/Requests/UpdateCheckupRecordRangeRequestDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOs.CheckUpRecordDTOs.Requests
+{
+    namespace DTOs.CheckUpRecordDTOs.Requests
+    {
+        public class UpdateCheckupRecordRangeRequestDTO
+        {
+            [Required, MinLength(1)]
+            public List<UpdateCheckupRecordRequestDTO> Records { get; set; } = [];
+        }
+    }
+
+}
