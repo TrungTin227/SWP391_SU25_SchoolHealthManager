f8196d26292c9731e9b1d9196ae00b229d577f27
TaiNguyen2108 <dinhtai2108@gmail.com>
2025-07-12 16:00:33 +0700

    fixed

--- Services/Implementations/VaccinationRecordService.cs
+++ Services/Implementations/VaccinationRecordService.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using DTOs.VaccinationRecordDTOs.Request;
-using DTOs.VaccinationRecordDTOs.Response;
 using Microsoft.Extensions.Logging;
 
 namespace Services.Implementations
