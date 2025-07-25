d360f3f300e964a60b539da6d95e2f5ebd77b4f0
TrungTin <tinvtse161572@fpt.edu.vn>
2025-07-25 18:42:12 +0700

    update database "UpdateVisionPropertiesToInt"

--- DTOs/CheckUpRecordDTOs/Requests/UpdateCheckupRecordRequestDTO.cs
+++ DTOs/CheckUpRecordDTOs/Requests/UpdateCheckupRecordRequestDTO.cs
@@ -15,8 +15,8 @@ namespace DTOs.CheckUpRecordDTOs.Requests
         public Guid Id { get; set; }
         public decimal? HeightCm { get; set; }
         public decimal? WeightKg { get; set; }
-        public VisionLevel? VisionLeft { get; set; }
-        public VisionLevel? VisionRight { get; set; }
+        public int? VisionLeft { get; set; }
+        public int? VisionRight { get; set; }
         public HearingLevel? Hearing { get; set; }
         public decimal? BloodPressureDiastolic { get; set; } //huyết áp 
         public Guid? ExaminedByNurseId { get; set; }              // Y tá thực hiện
