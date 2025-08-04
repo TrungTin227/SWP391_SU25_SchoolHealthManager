f5ddbeacd5d22db10305fe8a277008f96238d686
hctrung2k4 <hctrung2k4@gmail.com>
2025-08-04 21:29:33 +0700

    validate

--- Services/Implementations/ParentMedicationDeliveryService.cs
+++ Services/Implementations/ParentMedicationDeliveryService.cs
@@ -66,8 +66,8 @@ namespace Services.Implementations
                 var currentTime = _currentTime.GetVietnamTime();
                 if (currentTime.Hour < 7 || currentTime.Hour > 23)
                 {
-                    _logger.LogError("Thời gian hiện tại không hợp lệ. Chỉ cho phép tạo phiếu giao thuốc trong giờ làm việc (08:00 - 23:00)");
-                    return ApiResult<ParentMedicationDeliveryResponseDTO>.Failure(new ArgumentException("Chỉ cho phép tạo phiếu giao thuốc trong giờ làm việc (08:00 - 17:00)"));
+                    _logger.LogError("Thời gian hiện tại không hợp lệ. Chỉ cho phép tạo phiếu giao thuốc trong giờ làm việc (07:00 - 23:00)");
+                    return ApiResult<ParentMedicationDeliveryResponseDTO>.Failure(new ArgumentException("Chỉ cho phép tạo phiếu giao thuốc trong giờ làm việc (07:00 - 23:00)"));
                 }
 
                 // Validate từng medication
